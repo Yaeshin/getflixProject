c31b74c9f240a68dc9b4a8619b6b651ba88e9beb
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2023-11-30 16:57:11 +0100

    travail en cours sur la page profil

--- pages/profil_user.php
+++ pages/profil_user.php
@@ -8,10 +8,10 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="grid place-content-center min-h-screen   bg-slate-700 shadow-2xl">
-            <div class="bg-contain bg-center ">
-                <div class="grid grid-rows-3 grid-flow-col gap-4 bg-bg-slate-300 box-border h-150 w-150 p-4 border-4">
-                    <div class="row-span-3"> 
+<div class="grid min-h-screen h-80 grid place-content-center bg-slate-700 shadow-2xl">
+        <div class="justify-center border-2  rounded-md border-gray-700 p-7 mt-5 bg-slate-300 flex flex-col drop-shadow-2x shadow-lg shadow-indigo-500/40">
+                <div class="grid grid-rows-3 grid-flow-col gap-4 bg-bg-slate-300  h-150 w-150 p-4 ">
+                    <div class="row-span-3 mx-10"> 
                         <p>Profil</p>
                         <div class="flex items-center justify-center w-full">
                             <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
@@ -28,13 +28,14 @@
                             <input type="text" class="peer bg-slate-50 peer shrink appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white  focus:border-sky-500" id="inline-password" placeholder= "Joe" id="pseudo_inscription" name="pseudo_inscription"/>
                         </div>
                     </div>
-                    <div class="block ">
-                <label class="block text-black mb-1  mt-5 " for="email_inscription">Email</label>
-                <input type="email" class="peer mt-1 bg-slate-50 peer shrink appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white  focus:border-sky-500" id="inline-password" placeholder= "exemple@gmail.com" id="email_inscription" name="email_inscription"/>
-                <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-                  Please provide a valid email address.
-                </p>
-              </div>
+                    <div class="row-span-2 col-span-2">
+                        <div class="block ">
+                            <label class="block text-black mb-1  mt-5 " for="email_inscription">Email</label>
+                            <input type="email" class="peer mt-1 bg-slate-50 peer shrink appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white  focus:border-sky-500" id="inline-password" placeholder= "exemple@gmail.com" id="email_inscription" name="email_inscription"/>
+                            <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                            Please provide a valid email address.
+                            </p>
+                    </div>
               <div class="block ">
               <label class="block text-black mb-1 text-center" for="password_inscription">Password</label>
               <input class="bg-slate-50 peer shrink appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-sky-500 mt-1" id="inline-password" type="password" placeholder="******************" id="password_inscription" name="password_inscription">
@@ -44,6 +45,7 @@
               <label class="block text-black mb-1 mt-3"> Confirmation Password</label>
               <input class="bg-slate-50 peer shrink appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-sky-500 mt-1" id="inline-password" type="password" placeholder="******************">
               
+            </div>
             </div>
 
                     </div>
