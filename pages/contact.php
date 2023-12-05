de9131dce5aa2da33220e198f4e03d2157f336a7
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2023-12-05 16:56:24 +0100

    debut de la page contact

---
+++ pages/contact.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-700 ">
+
+   <div class="bg-slate-300 w-auto ">     
+        <div class="p-8 mt-8 max-w-lg mx-auto">
+            <h1 class="text-4xl text-gray-800 sm:text-4xl font-bold mb-4">Contact</h1>
+            <form>
+                <div class="p-2 w-full">
+                    <div class="relative">
+                    <label for="email" class="leading-7 py-4 text-lg text-gray-900">Email</label>
+                        <input type="email" id="email" name="email" required=""
+                            class="peer w-full bg-white rounded border border-gray-400 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-900 py-1 px-1 leading-8 transition-colors duration-200 ease-in-out">
+                            <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                            Please provide a valid email address.
+                            </p>
+             
+
+                </div>
+                <div class="p-2 w-full">
+                    <div class="relative">
+                    <div class="relative" data-te-dropdown-ref>
+  <button
+    class="flex items-center whitespace-nowrap rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none"
+    type="button"
+    id="dropdownMenuLargeButton"
+    data-te-dropdown-toggle-ref
+    aria-expanded="false"
+    data-te-ripple-init
+    data-te-ripple-color="light">
+    Large button
+    <span class="ml-2 w-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class="h-5 w-5">
+        <path
+          fill-rule="evenodd"
+          d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+          clip-rule="evenodd" />
+      </svg>
+    </span>
+  </button>
+  <ul
+    class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
+    aria-labelledby="dropdownMenuLargeButton"
+    data-te-dropdown-menu-ref>
+    <li>
+      <a
+        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+        href="#"
+        data-te-dropdown-item-ref
+        >Action</a
+      >
+    </li>
+    <li>
+      <a
+        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+        href="#"
+        data-te-dropdown-item-ref
+        >Another action</a
+      >
+    </li>
+    <li>
+      <a
+        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+        href="#"
+        data-te-dropdown-item-ref
+        >Something else here</a
+      >
+    </li>
+  </ul>
+</div>
+
+                    
+
+
+</div>
+                    </div>
+                </div>
+                <div class="p-2 w-full">
+                    <div class="relative">
+                        <label for="message" class="leading-7 py-4 text-lg text-gray-900">Message</label>
+                        <textarea id="message" name="message" required=""
+                            class=" w-full bg-white rounded border border-gray-400 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-900 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                    </div>
+                </div>
+                <div class="p-2 w-full">
+                    <div>
+                        <button type="submit"
+                            class="flex text-white bg-gray-900 border-0 py-3 px-6 focus:outline-none hover:bg-blue-900 rounded text-xl font-bold shadow-lg mx-0 flex-col text-center">
+                            Send
+                        </button>
+                    </div>
+            </form>
+        </div>
+   </div>
+   <script>
+    
+// Initialization for ES Users
+import {
+  Dropdown,
+  Ripple,
+  initTE,
+} from "tw-elements";
+
+initTE({ Dropdown, Ripple });
+   </script>
+</body>
+</html>
