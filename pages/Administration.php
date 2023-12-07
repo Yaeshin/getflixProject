b08fcd8314736a136a232e39503362735a2f989b
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2023-12-07 12:27:02 +0100

    debut de la page Administration

---
+++ pages/Administration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-700">
+    <!-- Insertion de la navbar -->
+    <?php include_once("./../styles/navAdmin.php"); ?>
+    
+
+</body>
+</html>
