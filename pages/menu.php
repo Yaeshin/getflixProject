28cf12e828c3cffaaff6c194262afd9cc860cea6
Thomas Syben <thomassyben07@gmail.com>
2023-12-08 13:00:19 +0100

    add menu selector

--- pages/menu.php
+++ pages/menu.php
@@ -16,6 +16,10 @@ $movies = [];
 while ($movie = $result->fetch_assoc()) {
     $movies[] = $movie;
 }
+
+// Variables pour suivre l'index de début et de fin des cartes affichées
+$startIndex = 0;
+$endIndex = 4;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -42,24 +46,24 @@ while ($movie = $result->fetch_assoc()) {
 <body class="w-screen h-screen bg-gray-600 overflow-x-hidden">
     <?php include "../styles/navBar.php"; ?>
     <main class="flex-1 flex flex-grow flex-col block">
-         <!-- Contenu de la deuxième section "Revoir" -->
+        <!-- Contenu de la première section "Pour Vous" -->
         <div class="w-full h-1/2 flex-grow">
-            <h1 class="w-full h-10vh text-white bg-gray-700 text-4xl text-center py-4">Pour vous</h1>
-            <div class="flex justify-around p-8">
-            <?php foreach ($movies as $movie): ?>
-                    <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
-                        <a href="film.php?id=<?php echo $movie['id_movie']; ?>" class="block w-full h-full bg-cover bg-center relative">
-                            <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
-                                <span class="text-white text-lg font-bold mb-2"><?php echo $movie['title']; ?></span>
-                                <span class="text-white text-center mx-5"><?php echo $movie['description']; ?></span>
-                            </div>
-                            <img src="<?php echo $movie['image']; ?>" alt="Image" class="w-full h-full object-cover">
-                        </a>
-                    </div>
-                <?php endforeach; ?>
+            <h1 class="w-full h-10vh text-white bg-gray-700 text-4xl text-center py-4">Pour Vous</h1>
+            <div class="flex justify-around items-center p-8 relative">
+                <button onclick="prev()" class="absolute left-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-l-lg focus:outline-none">
+                    <!-- Remplacement du texte par des symboles de flèches -->
+                    <img src="../img/selectorLeft.png" alt="left" class="w-1/2 pl-5 hover:scale-120">
+                </button>
+                <div class="carousel flex space-x-5">
+                    <!-- Les cartes de films seront générées ici -->
+                </div>
+                <button onclick="next()" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-r-lg focus:outline-none">
+                    <!-- Remplacement du texte par des symboles de flèches -->
+                    <img src="../img/selectorRight.png" alt="left" class="w-1/2 pr-5 hover:scale-120">
+                </button>
             </div>
-        </div>
 
+        </div>
 
         <!-- Contenu de la deuxième section "Populaires" -->
         <div class="w-full h-1/2 flex-grow">
@@ -97,17 +101,55 @@ while ($movie = $result->fetch_assoc()) {
         </div>
     </main>
     <script>
-        // Fonction pour naviguer vers la droite
-        function next() {
-            const container = document.querySelector('.flex.justify-around'); // Sélectionne le conteneur des cartes
-            container.scrollBy({ left: 320, behavior: 'smooth' }); // Fait défiler de 320px (largeur d'une carte) vers la droite
+        // Initialisation des variables
+        let startIndex = 0;
+        const movies = <?php echo json_encode($movies); ?>; // Assure-toi d'avoir les données JSON de tes films ici
+
+        // Fonction pour afficher les cinq cartes suivantes
+        function showNext() {
+            const cardContainer = document.querySelector('.carousel');
+            cardContainer.innerHTML = '';
+
+            for (let i = startIndex; i < startIndex + 5 && i < movies.length; i++) {
+                const movie = movies[i];
+                const card = `
+                    <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
+                        <a href="film.php?id=${movie.id_movie}" class="block w-full h-full bg-cover bg-center relative">
+                            <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
+                                <span class="text-white text-lg font-bold mb-2">${movie.title}</span>
+                                <span class="text-white text-center mx-5">${movie.description}</span>
+                            </div>
+                            <img src="${movie.image}" alt="Image" class="w-full h-full object-cover">
+                        </a>
+                    </div>
+                `;
+                cardContainer.innerHTML += card;
+            }
+
+            startIndex += 5;
         }
 
-        // Fonction pour naviguer vers la gauche
-        function prev() {
-            const container = document.querySelector('.flex.justify-around'); // Sélectionne le conteneur des cartes
-            container.scrollBy({ left: -320, behavior: 'smooth' }); // Fait défiler de 320px (largeur d'une carte) vers la gauche
+        // Fonction pour afficher les cinq cartes précédentes
+        function showPrev() {
+            startIndex -= 10;
+            if (startIndex < 0) {
+                startIndex = 0;
+            }
+            showNext();
         }
+
+        // Fonctions pour les boutons Précédent et Suivant
+        function next() {
+            showNext();
+        }
+
+        function prev() {
+            showPrev();
+        }
+
+        // Afficher les premières cinq cartes au chargement de la page
+        document.addEventListener('DOMContentLoaded', showNext);
+
     </script>
 </body>
 
