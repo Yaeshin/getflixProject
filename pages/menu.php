00382a878ed7e9f947a4041b1724eedf4dc3e024
Thomas Syben <thomassyben07@gmail.com>
2023-12-08 11:30:29 +0100

    add popular category

--- pages/menu.php
+++ pages/menu.php
@@ -42,16 +42,11 @@ while ($movie = $result->fetch_assoc()) {
 <body class="w-screen h-screen bg-gray-600 overflow-x-hidden">
     <?php include "../styles/navBar.php"; ?>
     <main class="flex-1 flex flex-grow flex-col block">
-        <!-- Contenu de la première section "Pour Vous" -->
+         <!-- Contenu de la deuxième section "Revoir" -->
         <div class="w-full h-1/2 flex-grow">
-            <h1 class="w-full h-10vh text-white bg-gray-700 text-4xl text-center py-4">Pour Vous</h1>
-            <div class="flex justify-around p-8 relative">
-                <button onclick="prev()" class="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-l-lg focus:outline-none">Précédent</button>
-                <?php 
-                $counter = 0; // Initialisation du compteur
-                foreach ($movies as $movie):
-                    if ($counter < 5): // Limite à 5 cartes affichées à la fois
-                ?>
+            <h1 class="w-full h-10vh text-white bg-gray-700 text-4xl text-center py-4">Pour vous</h1>
+            <div class="flex justify-around p-8">
+            <?php foreach ($movies as $movie): ?>
                     <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
                         <a href="film.php?id=<?php echo $movie['id_movie']; ?>" class="block w-full h-full bg-cover bg-center relative">
                             <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
@@ -61,12 +56,7 @@ while ($movie = $result->fetch_assoc()) {
                             <img src="<?php echo $movie['image']; ?>" alt="Image" class="w-full h-full object-cover">
                         </a>
                     </div>
-                <?php 
-                    endif;
-                    $counter++; // Incrémentation du compteur
-                    endforeach; 
-                ?>
-                <button onclick="next()" class="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-r-lg focus:outline-none">Suivant</button>
+                <?php endforeach; ?>
             </div>
         </div>
 
