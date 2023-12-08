2ef016ca4333779ff1860b86544cc7c5ad89052f
Thomas Syben <thomassyben07@gmail.com>
2023-12-08 15:02:07 +0100

    add independants slider

--- pages/menu.php
+++ pages/menu.php
@@ -50,19 +50,18 @@ $endIndex = 4;
         <div class="w-full h-1/2 flex-grow">
             <h1 class="w-full h-10vh text-white bg-gray-700 text-4xl text-center py-4">Pour Vous</h1>
             <div class="flex justify-around items-center p-8 relative">
-                <button onclick="prev()" class="absolute left-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-l-lg focus:outline-none">
+                <button onclick="prev('carouselPourVous')" class="absolute left-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-l-lg focus:outline-none flex items-center">
                     <!-- Remplacement du texte par des symboles de flèches -->
-                    <img src="../img/selectorLeft.png" alt="left" class="w-1/2 pl-5 hover:scale-120">
+                    <img src="../img/selectorLeft.png" alt="left" class="w-1/2 h-auto transition-transform hover:scale-125 mx-auto">
                 </button>
-                <div class="carousel flex space-x-5">
+                <div class="carousel flex" id="carouselPourVous">
                     <!-- Les cartes de films seront générées ici -->
                 </div>
-                <button onclick="next()" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-r-lg focus:outline-none">
+                <button onclick="next('carouselPourVous')" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-r-lg focus:outline-none flex items-center">
                     <!-- Remplacement du texte par des symboles de flèches -->
-                    <img src="../img/selectorRight.png" alt="left" class="w-1/2 pr-5 hover:scale-120">
+                    <img src="../img/selectorRight.png" alt="left" class="w-1/2 h-auto transition-transform hover:scale-125 mx-auto">
                 </button>
             </div>
-
         </div>
 
         <!-- Contenu de la deuxième section "Populaires" -->
@@ -70,74 +69,84 @@ $endIndex = 4;
             <?php  include_once 'populaires.php'; ?>
         </section>
 
-         <!-- Contenu de la deuxième section "Revoir" -->
+        <!-- Contenu de la deuxième section "Revoir" -->
         <div class="w-full h-1/2 flex-grow">
             <h1 class="w-full h-10vh text-white bg-gray-700 text-4xl text-center py-4">Revoir</h1>
-            <div class="flex justify-around p-8">
-            <?php foreach ($movies as $movie): ?>
-                    <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
-                        <a href="film.php?id=<?php echo $movie['id_movie']; ?>" class="block w-full h-full bg-cover bg-center relative">
-                            <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
-                                <span class="text-white text-lg font-bold mb-2"><?php echo $movie['title']; ?></span>
-                                <span class="text-white text-center mx-5"><?php echo $movie['description']; ?></span>
-                            </div>
-                            <img src="<?php echo $movie['image']; ?>" alt="Image" class="w-full h-full object-cover">
-                        </a>
-                    </div>
-                <?php endforeach; ?>
+            <div class="flex justify-around items-center p-8 relative">
+                <button onclick="prev('carouselRevoir')" class="absolute left-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-l-lg focus:outline-none flex items-center">
+                    <!-- Remplacement du texte par des symboles de flèches -->
+                    <img src="../img/selectorLeft.png" alt="left" class="w-1/2 h-auto transition-transform hover:scale-125 mx-auto">
+                </button>
+                <div class="carousel flex" id="carouselRevoir">
+                    <!-- Les cartes de films seront générées ici -->
+                </div>
+                <button onclick="next('carouselRevoir')" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-white px-3 py-2 rounded-r-lg focus:outline-none flex items-center">
+                    <!-- Remplacement du texte par des symboles de flèches -->
+                    <img src="../img/selectorRight.png" alt="left" class="w-1/2 h-auto transition-transform hover:scale-125 mx-auto">
+                </button>
             </div>
         </div>
     </main>
     <script>
-        // Initialisation des variables
-        let startIndex = 0;
-        const movies = <?php echo json_encode($movies); ?>; // Assure-toi d'avoir les données JSON de tes films ici
+        // Initialisation des données de films depuis PHP
+        const movies = <?php echo json_encode($movies); ?>;
 
-        // Fonction pour afficher les cinq cartes suivantes
-        function showNext() {
-            const cardContainer = document.querySelector('.carousel');
+        // Fonction pour générer les cartes de films
+        function generateMovieCard(movie) {
+            return `
+                <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
+                    <a href="film.php?id=${movie.id_movie}" class="block w-full h-full bg-cover bg-center relative">
+                        <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
+                            <span class="text-white text-lg font-bold mb-2">${movie.title}</span>
+                            <span class="text-white text-center mx-5">${movie.description}</span>
+                        </div>
+                        <img src="${movie.image}" alt="Image" class="w-full h-full object-cover">
+                    </a>
+                </div>
+            `;
+        }
+
+        // Fonction pour afficher les cartes suivantes pour un carrousel spécifique
+        function showNext(carouselId) {
+            const cardContainer = document.getElementById(carouselId);
             cardContainer.innerHTML = '';
 
+            const startIndex = cardContainer.dataset.startIndex ? parseInt(cardContainer.dataset.startIndex) : 0;
             for (let i = startIndex; i < startIndex + 5 && i < movies.length; i++) {
                 const movie = movies[i];
-                const card = `
-                    <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
-                        <a href="film.php?id=${movie.id_movie}" class="block w-full h-full bg-cover bg-center relative">
-                            <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
-                                <span class="text-white text-lg font-bold mb-2">${movie.title}</span>
-                                <span class="text-white text-center mx-5">${movie.description}</span>
-                            </div>
-                            <img src="${movie.image}" alt="Image" class="w-full h-full object-cover">
-                        </a>
-                    </div>
-                `;
+                const card = generateMovieCard(movie);
                 cardContainer.innerHTML += card;
             }
 
-            startIndex += 5;
+            cardContainer.dataset.startIndex = startIndex + 5;
         }
 
-        // Fonction pour afficher les cinq cartes précédentes
-        function showPrev() {
+        // Fonction pour afficher les cartes précédentes pour un carrousel spécifique
+        function showPrev(carouselId) {
+            const cardContainer = document.getElementById(carouselId);
+            let startIndex = cardContainer.dataset.startIndex ? parseInt(cardContainer.dataset.startIndex) : 0;
             startIndex -= 10;
             if (startIndex < 0) {
                 startIndex = 0;
             }
-            showNext();
+            cardContainer.dataset.startIndex = startIndex;
+            showNext(carouselId);
         }
 
         // Fonctions pour les boutons Précédent et Suivant
-        function next() {
-            showNext();
+        function next(carouselId) {
+            showNext(carouselId);
         }
 
-        function prev() {
-            showPrev();
+        function prev(carouselId) {
+            showPrev(carouselId);
         }
 
         // Afficher les premières cinq cartes au chargement de la page
-        document.addEventListener('DOMContentLoaded', showNext);
-
+        document.addEventListener('DOMContentLoaded', () => {
+            showNext('carouselPourVous');
+            showNext('carouselRevoir');
+        });
     </script>
 </body>
 
