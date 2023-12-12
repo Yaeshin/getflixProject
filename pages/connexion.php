455019fec18ec5d28eff532534d3141a05cd5daa
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2023-12-12 10:50:41 +0100

    modification du fichier connexion

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,4 +1,4 @@
-git<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -9,7 +9,7 @@ git<!DOCTYPE html>
 </head>
 
 <body>
- <div class="flex items-center justify-center h-screen bg-gray-800">
+    <div class="flex items-center justify-center h-screen bg-gray-800">
         <div class="w-11/12 md:w-4/6 lg:w-1/3 bg-gray-300 p-2 my-10 mx-auto shadow-2xl border-2 rounded-md bg-gray-300 flex flex-col drop-shadow-2x shadow-lg shadow-indigo-500/40">
             <div class="max-w-md my-3 opacity-90 mx-auto">
                 <div class="flex justify-center max-w-md my-3 opacity-90">
@@ -17,38 +17,38 @@ git<!DOCTYPE html>
                 </div>
                 <!-- Debut du formulaire -->
                 <form class="grid place-content-center" method="get">
-                            <div class="block ">
-                                <label class="block text-slate-700 mb-1 mt-10 sm:mt-10 text-center" for="email">Email</label>
-                                <input type="email" class="peer mt-1 bg-slate-50 peer appearance-none    w-full py-2 px-2 text-gray-700 leading-tight px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" placeholder="exemple@gmail.com" id="email" name="email" required autocomplete="on">
-                                <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
-                                    Please provide a valid email address.
-                                </p>
-                            </div>
-                            <div class="block ">
-                                <label class="block text-slate-700 mb-1 mt-10 sm:mt-8 text-center" for="password">Password</label>
-                                <input class="bg-slate-50 peer appearance-none mt-1 bg-slate-50 peer appearance-none    w-full py-2 px-2 text-gray-700 leading-tight px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" type="password" placeholder="******************" id="password" name="password">
-                            </div>
-                            <div class="mt-10 sm:mt-10 flex justify-center">
-                                <button class="bg-sky-500 hover:bg-sky-700 py-1 px-3 text-xl mx-auto mt-10 text-white rounded transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 shadow-lg shadow-blue-500/50" type="submit" valeur="connexion">
-                                    Connexion
-                                </button>
-                            </div>
-                            <div class="text-center mt-10 sm:mt-10">
-                                <a href="inscription.php" class="hover:text-blue-500 hover:underline
-                                hover:text-blue-500 hover:font-bold">
-                                    <p>S'inscrire</p>
-                                </a>
-                            </div>
-                            <div class="mt-8 text-center">
-                                <a href="" class="hover:text-blue-500 hover:underline hover:text-blue-500 hover:font-bold" >
-                                    <p>Mot de passe oublié ?</p>
-                                </a>
-                            </div>
-                        </form>
+                    <div class="block ">
+                        <label class="block text-slate-700 mb-1 mt-10 sm:mt-10 text-center" for="email">Email</label>
+                        <input type="email" class="peer mt-1 bg-slate-50 peer appearance-none    w-full py-2 px-2 text-gray-700 leading-tight px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" placeholder="exemple@gmail.com" id="email" name="email" required autocomplete="on">
+                        <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+                            Please provide a valid email address.
+                        </p>
                     </div>
-                </div>
+                    <div class="block ">
+                        <label class="block text-slate-700 mb-1 mt-10 sm:mt-8 text-center" for="password">Password</label>
+                        <input class="bg-slate-50 peer appearance-none mt-1 bg-slate-50 peer appearance-none    w-full py-2 px-2 text-gray-700 leading-tight px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-500" type="password" placeholder="******************" id="password" name="password">
+                    </div>
+                    <div class="mt-10 sm:mt-10 flex justify-center">
+                        <button class="bg-sky-500 hover:bg-sky-700 py-1 px-3 text-xl mx-auto mt-10 text-white rounded transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 shadow-lg shadow-blue-500/50" type="submit" valeur="connexion">
+                            Connexion
+                        </button>
+                    </div>
+                    <div class="text-center mt-10 sm:mt-10">
+                        <a href="inscription.php" class="hover:text-blue-500 hover:underline
+                                hover:text-blue-500 hover:font-bold">
+                            <p>S'inscrire</p>
+                        </a>
+                    </div>
+                    <div class="mt-8 text-center">
+                        <a href="" class="hover:text-blue-500 hover:underline hover:text-blue-500 hover:font-bold">
+                            <p>Mot de passe oublié ?</p>
+                        </a>
+                    </div>
+                </form>
             </div>
-            </div>
+        </div>
+    </div>
+    </div>
 </body>
 
 </html>
