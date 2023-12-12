c60786faf23a980ae026ba0784a23a0f67c9b25a
Cedric Berthet <berthetcedric@gmail.com>
2023-12-12 16:47:07 +0100

    moteur de recherche

--- styles/navBar.php
+++ styles/navBar.php
@@ -47,8 +47,10 @@
 
     <!-- Right side with search bar -->
     <div class="flex items-center">
-        <input type="text" placeholder="Search..." class="px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-500">
-        <button class="ml-2 px-3 py-1 bg-blue-500 text-white rounded">Search</button>
+        <form action="search_results.php" method="post">
+            <input type="text" name="search_term" placeholder="Search..." class="px-2 py-1 rounded border border-gray-300 focus:outline-none focus:ring focus:border-blue-500">
+            <button class="ml-2 px-3 py-1 bg-blue-500 text-white rounded">Search</button>
+        </form>
     </div>
 </header>
 
