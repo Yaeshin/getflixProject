1a8ab2b712d408bba268addd85f01eb990a2cbb8
Thomas Syben <thomassyben07@gmail.com>
2023-12-13 14:04:11 +0100

    add style to index

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $randomMovies = $randomMoviesResult->fetch_all(MYSQLI_ASSOC);
             <h2 class="text-white text-2xl font-bold mb-4 text-center" id="popupTitle">Se connecter ?</h2>
             <p class="text-white text-xl my-8 text-center mx-6" id="popupDescription">Profiter de tout notre catalogue en vous connectant juste ici.</p>
             <div class="flex justify-around">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded hover:scale-110" id="popupButton1"><a href="pages/connexion.php">Se connecter</a></button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded hover:scale-110" id="popupButton1"><a href="auth.php?action=login">Se connecter</a></button>
                 <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-8 rounded hover:scale-110" id="popupButton2"><a href="index.php">Quitter</a></button>
             </div>
         </div>
@@ -51,9 +51,9 @@ $randomMovies = $randomMoviesResult->fetch_all(MYSQLI_ASSOC);
     <?php include "styles/navAno.php"; ?>
     <main class="flex-1 flex flex-grow flex-col block">
         <div class="w-full h-1/2 flex-grow">
-            <div class="flex justify-around p-8">
+            <div class="flex flex-wrap justify-center p-8">
                 <?php foreach ($randomMovies as $movie): ?>
-                    <div class="w-64 h-96 mx-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
+                    <div class="w-64 h-96 mx-5 my-5 relative overflow-hidden rounded-lg transform transition-transform hover:scale-105">
                         <a href="film.php?id=<?php echo $movie['id_movie']; ?>" class="popupLink block w-full h-full bg-cover bg-center relative">
                             <div class="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 hover:opacity-90 flex flex-col justify-center items-center">
                                 <span class="text-white text-lg font-bold mb-2"><?php echo $movie['title']; ?></span>
