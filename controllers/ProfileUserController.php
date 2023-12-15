ecb573bd2ec943e11294d8d67ad8104906f1dc23
Celestin Sivixay <celestin42@hotmail.fr>
2023-12-15 10:08:38 +0100

    correct edit profil

--- controllers/ProfileUserController.php
+++ controllers/ProfileUserController.php
@@ -21,7 +21,7 @@ class ProfileUserController{
                 $newNick=$_POST['newNick'];
             }
             if(!empty($_POST['newPw'])){
-                if($_POST['newPw']==$_POST['confirmNewPw'])$newNick=$_POST['newPw'];
+                if($_POST['newPw']==$_POST['confirmNewPw'])$newPew=$_POST['newPw'];
                 else {
                     $notification="Passwords don't match";
                     include('profil_user.php');
